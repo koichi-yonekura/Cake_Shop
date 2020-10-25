5b874083db832b7a211e7db414c8815e63f47f9a
koichi-yonekura <asari0001z@gmail.com>
2020-10-26 00:26:25 +0900

    [Add] Model&DateBase作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :publics
+  namespace :admin do
+    get 'items/new'
+    get 'items/create'
+    get 'items/index'
+    get 'items/show'
+    get 'items/edit'
+    get 'items/update'
+  end
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+}
+
+  devise_for :customers, controllers: {
+  	sessions:      'customers/sessions',
+    passwords:     'customers/passwords',
+    registrations: 'customers/registrations'
+}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
+  end
 end
