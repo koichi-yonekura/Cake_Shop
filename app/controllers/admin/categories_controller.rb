8c6ebba69d7d632e56fafbeae19362495dcde184
koichi-yonekura <asari0001z@gmail.com>
2020-10-28 23:58:59 +0900

    [Add] category画面の作成

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,21 +1,28 @@
 class Admin::CategoriesController < ApplicationController
   def index
   	@category = Category.new
+  	@categories = Category.all
   end
 
   def create
   	@category = Category.new(category_params)
+  	@categories = Category.all
+  	@category.save
+  	redirect_to admin_categories_path
   end
 
   def edit
+  	@category = Category.find(params[:id])
   end
 
   def update
+  	@category = Category.find(params[:id])
+  	redirect_to admin_categories_path
   end
 
   private
   def category_params
-    params.require(:category).permit(:name)
+    params.require(:category).permit(:name, :is_active)
   end
 
 end
